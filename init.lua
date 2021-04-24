2b204cb95a0b63887ed93023973d57b90dd4f34e
Mingming Cui <Angel_tommy@foxmail.com>
2021-04-25 00:00:25 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ registerForEvent("onInit", function()
 	loop = true
 end)
 
-registerForEvent("onUpdate", function(dt)
+registerForEvent("onUpdate", function()
 	if loop then
 		if Game.GetQuestsSystem():GetFactStr("q000_started") == 0 then
 			GetSingleton("inkMenuScenario"):GetSystemRequestsHandler():LoadSavedGame(0)
